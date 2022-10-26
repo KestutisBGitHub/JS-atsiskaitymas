2d979afacb1cf2d80b7d88a703ba5ef1404708c8
KestutisBGitHub <k.bacevicius@gmail.com>
2022-10-26 21:59:40 +0300

    task 3 complete with style

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,4 +11,48 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+const crateCard = element => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.style.minWidth = "380px";
+  card.style.maxWidth = "100%";
+  card.style.border = "1px solid black";
+  card.style.borderRadius = "25px";
+  card.style.padding = "10px";
+  card.style.margin = "5px";
+  card.style.flex = "1";
+
+  const login = document.createElement("p");
+  login.textContent = `User login name: ${element.login}`;
+
+  const avatarURL = document.createElement("p");
+  avatarURL.textContent = `Avatar URL: ${element.avatar_url}`;
+
+  card.append(login, avatarURL);
+  document.getElementById("output").append(card);
+};
+
+const getUsers = async () => {
+  try {
+    const response = await fetch(ENDPOINT);
+    const data = await response.json();
+
+    data.forEach(element => crateCard(element));
+
+    console.log(data); // trinti
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+document.getElementById("btn").addEventListener("click", e => {
+  e.preventDefault();
+  document.getElementById("btn").disabled = true;
+  document.getElementById("message").style.display = "none";
+  document.getElementById("output").style.display = "flex";
+
+  document.getElementById("output").style.flexWrap = "wrap";
+  getUsers();
+});
