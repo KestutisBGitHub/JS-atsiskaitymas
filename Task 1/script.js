6ddea17c53388e3fc6aca853f2d3a4d23e76fad4
KestutisBGitHub <k.bacevicius@gmail.com>
2022-10-26 20:30:39 +0300

    task 1 coding done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,33 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const myForm = document
+  .querySelector("form")
+  .addEventListener("submit", event => {
+    event.preventDefault();
+
+    const weightKG = document.getElementById("search").value;
+
+    const weightLBs = Math.round(weightKG * 2.2046 * 100) / 100;
+    const resultLbs = document.createElement("p");
+    resultLbs.textContent = "Pounds " + weightLBs + " lb";
+
+    const weightGrams = Math.round(weightKG / 0.001);
+    const resultGrams = document.createElement("p");
+    resultGrams.textContent = `Grams ${weightGrams} g`;
+
+    const weightOz = Math.round(weightKG * 35.274 * 100) / 100;
+    const resultOz = document.createElement("p");
+    resultOz.textContent = `Ounces ${weightOz} Oz`;
+
+    const text = "Your weight in:";
+
+    document
+      .getElementById("output")
+      .append(text, resultLbs, resultGrams, resultOz);
+
+    console.log(weightLBs);
+    console.log(weightGrams);
+    console.log(weightOz);
+  });
