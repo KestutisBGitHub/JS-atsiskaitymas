ccc2686678bb52ccccff2dee3acbfbc880ada1bc
KestutisBGitHub <k.bacevicius@gmail.com>
2022-10-27 20:26:48 +0300

    task 9 done

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,22 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive() {
+    if (this.budget >= 100000000) {
+      console.log("True");
+    } else {
+      console.log("False");
+    }
+  }
+}
+
+const movie = new Movie("The Matrix", "the Wachowskis", 63000000);
+
+movie.wasExpensive();
